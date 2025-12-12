9ba4940e8e1d3b369e83b66572419ac1a5a26c6e
sumit vikram <vikramsumit@outlook.com>
2025-12-12 17:58:56 +0530

    Initial commit of my Java codes

---
+++ Basics/javabasics4.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class javabasics4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        do {
+            System.out.println("enter your required no:");
+            int n = sc.nextInt();
+
+            if (n % 13 == 0) {
+                break;
+            }
+            System.out.println(n);
+        } while (true);
+
+        System.out.println("Ab ruk ja bhai");
+    }
+}
