e63ce0395977457649999115cc2a0e3ba54ca888
sumit vikram <vikramsumit@outlook.com>
2025-12-24 01:57:31 +0530

    Enhancement

--- java_questions/swap.java
+++ java_questions/swap.java
@@ -2,19 +2,19 @@
 
 public class swap {
     public static void swap(int a, int b) {
-        
-        int temopu=a;
-        a=b;
-        b=temopu;
 
-        System.out.println("a=" + a);
-        System.out.println("b=" + b);
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
 
     }
+
     public static void main(String[] args) {
-        int a=55;
-        int b=44;
-        swap(a,b);
-        
+        int a = 55;
+        int b = 44;
+        swap(a, b);
     }
 }
