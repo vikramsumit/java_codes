ca87c5972567067f5138711d220787a5de89d8c4
sumit vikram <vikramsumit@outlook.com>
2026-03-02 18:00:04 +0530

    safe_coding

--- llp1/ll1.java
+++ llp1/ll1.java
@@ -1,4 +1,7 @@
+
+
 public class ll1 {
+
     public static class Node{
         int data;
         Node next;
@@ -17,7 +20,9 @@ public class ll1 {
     
         // create new node
         Node newNode = new Node(data);
+
         size++;
+
         if (head == null) {
             head = tail = newNode;
             return;
@@ -28,6 +33,24 @@ public class ll1 {
 
         // step3 - head = newNode
         head = newNode;
+    }
+
+    public void addLast(int data){
+
+        Node newNode = new Node(data);
+
+        size++;
+
+        if(head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        // assign new node next to tail
+        tail.next = newNode;
+
+        // making last node tail
+        tail = newNode;
     }
 
     public void print() {
@@ -49,7 +72,14 @@ public class ll1 {
         ll.addFirst(89);
         ll.addFirst(88);
         ll.addFirst(87);
-        ll.addFirst(84);
+        ll.addFirst(86);
+
+        ll.print();
+
+        ll.addLast(88);
+        ll.addLast(87);
+        ll.addLast(86);
+        ll.addLast(85);
 
         ll.print();
     }
