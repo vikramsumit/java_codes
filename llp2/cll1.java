52818e5fe6d86085a921af63080032675f6bfeb0
sumit vikram <vikramsumit@outlook.com>
2026-03-04 23:09:13 +0530

    cycle_in_ll

---
+++ llp2/cll1.java
@@ -0,0 +1,41 @@
+import org.w3c.dom.Node;
+
+public class cll1 {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    // public static int size;
+
+    public static boolean isCycle() { // Floyd's CFA
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next; // +1
+            fast = fast.next.next; // +2
+            if (slow == fast) {
+                return true; // cycle exist
+            }
+        }
+        return false; // cycle doesnot exist
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head;
+
+        System.out.println(isCycle());
+    }
+}
