59754cc111817f68f3a34381deda219327109c73
sumit vikram <vikramsumit@outlook.com>
2026-03-05 23:47:01 +0530

    cycle_in_ll

---
+++ llp2/cll2.java
@@ -0,0 +1,56 @@
+public class cll2 {
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    // public static int size;
+
+public static void removeCycle() {
+        // detec₺cycle
+        Node slow = head;
+        Node fast = head;
+        boolean cycle = false;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                cycle = true;
+                break;
+            }
+        }
+        if (cycle == false) {
+            return;
+        }
+
+        // find meeting point
+        slow = head;
+        Node prev = null;
+        while (slow != fast) {
+            prev = fast;
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        // remove cycle -> fast.prev -> null
+        prev.next = null;
+    }
+
+    public static void main(String[] args) {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = head;
+
+        System.out.println(isCycle());
+    }
+}
