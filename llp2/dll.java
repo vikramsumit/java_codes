6ff39e55bc22081150e7f42c5cf660ef96f39cb9
sumit vikram <vikramsumit@outlook.com>
2026-03-15 00:34:30 +0530

    dll

---
+++ llp2/dll.java
@@ -0,0 +1,76 @@
+public class dll {
+    public class Node {
+        int data;
+        Node prev;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    // add
+    public void addFirst(int data) {
+        Node newNode = new Node(data);
+        size++;
+
+        if (head == null) {
+            head = tail = newNode;
+            return;
+        }
+
+        newNode.next = head;
+        head.prev = newNode;
+        head = newNode;
+    }
+
+    // print
+    public void print() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "<->");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public int removeFirst() {
+        if (head == null) {
+            System.out.println("DLL is empty");
+            return Integer.MIN_VALUE;
+        }
+
+        if (size == 1) {
+            int val = head.data;
+            head = tail = null;
+            size--;
+            return val;
+            
+        }
+        int val = head.data;
+        head = head.next;
+        head.prev = null;
+        size--;
+        return val;
+    }
+
+    public static void main(String[] args) {
+        dll list = new dll();
+
+        list.addFirst(3);
+        list.addFirst(99);
+        list.addFirst(8);
+        list.addFirst(5);
+
+        list.print();
+        System.out.println(list.size);
+
+        
+    }
+}
