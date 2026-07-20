8e42fa0fe4e355370507c6e17bca94046f7aa6b2
sumit vikram <vikramsumit@outlook.com>
2026-07-20 06:28:22 +0530

    Tree traversals

--- Binary_Tree/bin1.java
+++ Binary_Tree/bin1.java
@@ -96,6 +96,14 @@ public class bin1 {
     public static void main(String[] args) {
         int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
 
+        /*
+                  1       
+                /   \
+               2     3
+              / \     \
+             4   5     6
+
+        */
         BinaryTree tree = new BinaryTree();
         Node root = tree.buildTree(nodes);
         // System.out.println(root.data);
