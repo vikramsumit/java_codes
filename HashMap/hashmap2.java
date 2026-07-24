dd9ec83c0f1f273c6f125103e09b7dd958cf23ce
sumit vikram <vikramsumit@outlook.com>
2026-07-25 05:28:08 +0530

    LinkedHashMap

---
+++ HashMap/hashmap2.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+// import java.util.TreeMap;
+
+public class hashmap2 {
+    public static void main(String[] args) {
+        LinkedHashMap<String, Integer> lhm = new LinkedHashMap();
+        lhm.put("India", 150);
+        lhm.put("China", 140);
+        lhm.put("US", 30);
+
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("India", 150);
+        hm.put("China", 140);
+        hm.put("US", 30);
+
+        System.out.println(hm);
+        System.out.println(lhm);
+    }
+}
---
+++ HashMap/treemap.java
@@ -0,0 +1,17 @@
+//Red Black Tree --> Read it
+import java.util.*l
+
+public class treemap {
+    public static void main(String[] args) {
+        TreeMap<String, Integer> tm = new TreeMap();
+        tm.put("India", 150);
+        tm.put("China", 140);
+        tm.put("US", 30);
+
+        HashMap<String, Integer> hm = new HashMap<>();
+        hm.put("India", 150);
+        hm.put("China", 140);
+        hm.put("US", 30);
+        
+    }
+}
