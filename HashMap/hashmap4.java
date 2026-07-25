5308be5444b94813cce1d3fde33f5331b6a56688
sumit vikram <vikramsumit@outlook.com>
2026-07-25 14:17:54 +0530

    valid anagram

---
+++ HashMap/hashmap4.java
@@ -0,0 +1,43 @@
+//VALID ANAGRAM
+
+import java.util.*;
+
+public class hashmap4 {
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        HashMap<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+            // map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+
+            if (map.get(ch) != null) {
+                if (map.get(ch) == 1) {
+                    map.remove(ch);
+                } else {
+                    map.put(ch, map.get(ch) - 1);
+                }
+            } else {
+                return false;
+            }
+        }
+
+        return map.isEmpty();
+
+    }
+
+    public static void main(String[] args) {
+        String s = "rajurace", t = "careraju";
+
+        System.out.println(isAnagram(s, t));
+        // isAnagram(s, t);
+    }
+}
