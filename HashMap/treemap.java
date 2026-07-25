ad52022eacc977b5601dae6dd1d664b441a08b7a
sumit vikram <vikramsumit@outlook.com>
2026-07-25 08:46:12 +0530

    treemap

--- HashMap/treemap.java
+++ HashMap/treemap.java
@@ -6,11 +6,13 @@ public class treemap {
         TreeMap<String, Integer> tm = new TreeMap<>();
         tm.put("India", 150);
         tm.put("China", 140);
+        tm.put("Italy", 7);
         tm.put("US", 30);
-
+        
         HashMap<String, Integer> hm = new HashMap<>();
         hm.put("India", 150);
         hm.put("China", 140);
+        hm.put("Italy", 7);
         hm.put("US", 30);
 
         System.out.println(tm);
