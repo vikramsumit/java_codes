f88d2e42a2d6a84795cba9bf3c508959dfb22756
sumit vikram <vikramsumit@outlook.com>
2026-07-27 08:39:48 +0530

    fibonacci

--- Basics/RecursionBasics.java
+++ Basics/RecursionBasics.java
@@ -1,3 +1,22 @@
 public class RecursionBasics {
+    public static int fibonacci(int n){
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+
+        return fibonacci( n - 1) + fibonacci(n - 2);
+    }
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i <= n; i++) {
+            System.out.print(fibonacci(i) + " ");
+            
+        }
+
+    }
     
 }
