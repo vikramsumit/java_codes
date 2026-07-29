8c3690bc3f638776bb9ec50a7fc88373fa0854c7
sumit vikram <vikramsumit@outlook.com>
2026-07-29 11:20:27 +0530

    union and intersection

--- HashMap/problem2.java
+++ HashMap/problem2.java
@@ -1,4 +1,4 @@
-//Union ans INtersection
+// Union and Intersection
 
 import java.util.HashSet;
 
@@ -6,9 +6,10 @@ public class problem2 {
     public static void main(String[] args) {
         int arr1[] = {7, 3, 9};
         int arr2[] = {6, 3, 9, 2, 9, 4};
+
         HashSet<Integer> set = new HashSet<>();
 
-        //UNION
+        // UNION
         for (int i = 0; i < arr1.length; i++) {
             set.add(arr1[i]);
         }
@@ -19,23 +20,26 @@ public class problem2 {
 
         System.out.println("Union = " + set.size());
         System.out.println(set);
-        
-        //INTERSECTION
+
+        // INTERSECTION
         set.clear();
-        
+
         for (int i = 0; i < arr1.length; i++) {
             set.add(arr1[i]);
         }
-        
+
+        HashSet<Integer> intersection = new HashSet<>();
         int count = 0;
+
         for (int i = 0; i < arr2.length; i++) {
             if (set.contains(arr2[i])) {
                 count++;
-                set.remove(arr2[i]);
+                intersection.add(arr2[i]);
+                set.remove(arr2[i]); // avoids counting duplicates
             }
         }
-        
+
         System.out.println("Intersection = " + count);
-        System.out.println(set);
+        System.out.println(intersection);
     }
 }
